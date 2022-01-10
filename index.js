21cbfea73c7e1256945050070baba91aaa498e59
Scarecrow2905 <tommy-sc@live.no>
2022-01-10 13:09:29 +0100

    Nye oppgaver
    OnClick

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+var age = 28;
+var firstName ="Tommy";
+var lastName = "Scarecrow"
+var online = true;
+var whatever;
+
+/*var myCoolVariable "CamelCase"*/
+
+
+age = age + 1
+var fullName = firstName + lastName;
+
+console.log("Your age is",age);
+console.log("Hello",fullName);
+console.log("Are you online", online);
+console.log(whatever);
+
+let mySweetVariable = 420; //En midlertidig variabel "let"
+const pi = 3.14159; //En konstant value som ikke kan endres
+
+/* 
+     arithmetic expression is some combination of operands /values, variables, etc.)
+     operators (+ - * /) that can evaluated to a value
+*/
+
+   
